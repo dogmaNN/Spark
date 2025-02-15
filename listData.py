58f20f252ad6d265d737445362ff6a9cdd775ebe
dogmaNN <agaghihi7gg@gmail.com>
2025-02-16 07:55:44 +0800

    add wordList

--- listData.py
+++ listData.py
@@ -1,6 +1,7 @@
 #listData / __init__.py
 
-#wordList = []
+wordList = ["好啊沒關係啊我沒差啊我就閉嘴啊你繼續找理由啊我不在乎啊反正每個人都這樣啊我已經習慣了啦真的沒關係啊其他人開心就好了不用在乎我真的啦我就是沒人愛啦"]
+
 
 memeList = ["https://tenor.com/view/bee-eyebrow-raise-vurge-bee-gif-6443281501236067345",
             "https://tenor.com/view/rock-one-eyebrow-raised-rock-staring-the-rock-gif-22113367",
